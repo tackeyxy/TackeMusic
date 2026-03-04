bdabed3b995ff455965f332f85f4482ad42991fc
tacke <tackeyxy@gmail.com>
2026-03-04 18:40:03 +0800

    Update VersionInfo.kt

--- app/src/main/java/com/tacke/music/data/model/VersionInfo.kt
+++ app/src/main/java/com/tacke/music/data/model/VersionInfo.kt
@@ -1,14 +1,19 @@
 package com.tacke.music.data.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class VersionInfo(
     val versionCode: Int,
     val versionName: String,
+    @SerialName("download_url")
     val downloadUrl: String,
+    @SerialName("file_size")
     val fileSize: String,
+    @SerialName("release_notes")
     val releaseNotes: String,
+    @SerialName("publish_time")
     val publishTime: String
 )
 
